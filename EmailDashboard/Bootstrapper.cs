856da32da4d4f3e8722e7aaf28d25447f69e0554
Avneesh Verma <avneesh_verma@yahoo.com>
2011-10-06 23:47:36 +0100

    Project setup with Prism4

---
+++ EmailDashboard/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Windows;
+using EmailDashboard.Login;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace EmailDashboard
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.RootVisual = (UIElement)this.Shell;
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(LoginModule));
+        }
+
+
+    }
+}
